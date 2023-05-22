38e850d852f6c20be7492ea75de7f0f246c3351a
Luis Lacasa <luis.lacasa@microchip.com>
2023-05-22 14:12:52 +0200

    SMS-3457: Added MAC-RT PLC&Go application with FreeRTOS for PIC32CXMTG-EK

---
+++ apps/mac_rt_apps/mac_rt_plc_and_go/firmware/src/config/pic32cx_mtg_ek_pl460_freertos/driver/plc/g3MacRt/drv_g3_macrt_local.h
@@ -0,0 +1,189 @@
+/*******************************************************************************
+  G3 MAC RT Driver Local Data Structures
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    drv_g3_macrt_local.h
+
+  Summary:
+    G3 MAC RT Driver Local Data Structures
+
+  Description:
+    G3 MAC Real Time Driver Local Data Structures
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef _DRV_G3_MACRT_LOCAL_H
+#define _DRV_G3_MACRT_LOCAL_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "system/system.h"
+#include "driver/plc/g3MacRt/drv_g3_macrt.h"
+#include "driver/plc/g3MacRt/drv_g3_macrt_definitions.h"
+#include "driver/plc/g3MacRt/drv_g3_macrt_comm.h"
+#include "driver/plc/common/drv_plc_boot.h"
+#include "osal/osal.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* DRV_G3_MACRT Command set
+
+  Summary:
+    Enumeration listing the DRV_G3_MACRT commands.
+
+  Description:
+    This enumeration defines the commands used to interact with the DRV_G3_MACRT
+    series of devices.
+
+  Remarks:
+    None
+*/
+
+typedef enum
+{
+    /* Write command */
+    DRV_G3_MACRT_CMD_WRITE              = (1 << 10),
+
+    /* Read command */
+    DRV_G3_MACRT_CMD_READ               = (0 << 10)
+
+} DRV_G3_MACRT_CMD;
+
+// *****************************************************************************
+/* PLC Driver Instance Object
+
+  Summary:
+    Object used to keep any data required for an instance of the PLC driver.
+
+  Description:
+    None.
+
+  Remarks:
+    None.
+*/
+
+typedef struct
+{
+    /* State of the MAC RT driver */
+    DRV_G3_MACRT_STATE                        state;
+
+    /* HAL API list that will be used by the driver to access the hardware */
+    DRV_PLC_HAL_INTERFACE                     *plcHal;
+
+    /* PLC Profile */
+    uint8_t                                   plcProfile;
+
+    /* Size (in Bytes) of the PLC binary file */
+    uint32_t                                  binSize;
+
+    /* Address where PLC binary file is located */
+    uint32_t                                  binStartAddress;
+
+    /* Secure mode */
+    bool                                      secure;
+
+    /* Sleep mode */
+    bool                                      sleep;
+
+    /* PLC Initialization Confirm Callback */
+    DRV_G3_MACRT_INIT_CALLBACK                initCallback;
+	
+    /* Application Bootloader Data Callback */
+    DRV_PLC_BOOT_DATA_CALLBACK                bootDataCallback;
+
+    /* Application Transmission Confirm Callback */
+    DRV_G3_MACRT_TX_CFM_CALLBACK              txCfmCallback;
+
+    /* Application Data Indication Callback */
+    DRV_G3_MACRT_DATA_IND_CALLBACK            dataIndCallback;
+
+    /* Application RX Parameters Indication Callback */
+    DRV_G3_MACRT_RX_PARAMS_IND_CALLBACK       rxParamsIndCallback;
+
+    /* Application MAC RT Sniffer Indication Callback */
+    DRV_G3_MACRT_MAC_SNIFFER_IND_CALLBACK     macSnifferIndCallback;
+
+    /* Application Comm Status Indication Callback */
+    DRV_G3_MACRT_COMM_STATUS_IND_CALLBACK     commStatusIndCallback;
+
+    /* Application PLC PHY Sniffer Callback */
+    DRV_G3_MACRT_PHY_SNIFFER_IND_CALLBACK     phySnifferIndCallback;
+
+    /* Application Sleep Indication Callback */
+    DRV_G3_MACRT_SLEEP_IND_CALLBACK           sleepIndCallback;
+
+    /* Application Exception Callback */
+    DRV_G3_MACRT_EXCEPTION_CALLBACK           exceptionCallback;
+    
+    /* Event detection flag: data of new reception */
+    volatile uint16_t                         evDataIndLength;
+
+    /* Event detection flag: length of the response with register content */
+    volatile uint16_t                         evRegRspLength;
+
+    /* Event detection flag: MAC Sniffer */
+    volatile uint16_t                         evMacSnifLength;
+
+    /* Event detection flag: PHY Sniffer */
+    volatile uint16_t                         evPhySnifLength;
+
+    /* Event detection flag: reset waiting tx cfm */
+    volatile bool                             evResetTxCfm;
+
+    /* Event detection flag: tx cfm */
+    volatile bool                             evTxCfm;
+
+    /* Event detection flag: Comm Status */
+    volatile bool                             evCommStatus;
+
+    /* Event detection flag: RX parameters */
+    volatile bool                             evRxParams;
+    
+    /* Pointer to MAC Sniffer Data Buffer */
+    uint8_t                                   *pMacDataSniffer;
+    
+    /* Pointer to PHY Sniffer Data Buffer */
+    uint8_t                                   *pPhyDataSniffer;
+
+    /* Semaphore identifier. Used to suspend and resume task */
+    OSAL_SEM_DECLARE(semaphoreID);
+
+} DRV_G3_MACRT_OBJ;
+
+
+#endif //#ifndef _DRV_G3_MACRT_LOCAL_H
---
+++ apps/mac_rt_apps/mac_rt_plc_and_go/firmware/src/config/pic32cx_mtg_ek_pl460_freertos/initialization.c
@@ -0,0 +1,340 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+#pragma config SECURITY_BIT = CLEAR
+#pragma config BOOT_MODE = 0x3
+#pragma config PLANE_SELECTION = CLEAR
+#pragma config ERASE_FUNCTION_LOCK = 0x0
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+// <editor-fold defaultstate="collapsed" desc="DRV_PLC_HAL Initialization Data">
+
+/* HAL Interface Initialization for PLC transceiver */
+DRV_PLC_PLIB_INTERFACE drvPLCPlib = {
+
+    /* SPI Transfer Setup */
+    .spiPlibTransferSetup = (DRV_PLC_SPI_PLIB_TRANSFER_SETUP)FLEXCOM5_SPI_TransferSetup,
+
+    /* SPI Is Busy */
+    .spiIsBusy = FLEXCOM5_SPI_IsTransmitterBusy,
+
+    /* SPI Write/Read */
+    .spiWriteRead = FLEXCOM5_SPI_WriteRead,
+
+    
+    /* SPI clock frequency */
+    .spiClockFrequency = DRV_PLC_SPI_CLK,
+    
+    /* PLC LDO Enable Pin */
+    .ldoPin = DRV_PLC_LDO_EN_PIN, 
+    
+    /* PLC Reset Pin */
+    .resetPin = DRV_PLC_RESET_PIN,
+       
+    /* PLC External Interrupt Pin */
+    .extIntPin = DRV_PLC_EXT_INT_PIN,
+
+    /* PLC TX Enable Pin */
+    .txEnablePin = DRV_PLC_TX_ENABLE_PIN,
+    
+    /* PLC StandBy Pin */
+    .stByPin = DRV_PLC_STBY_PIN,
+    
+    /* PLC External Interrupt Pin */
+    .thMonPin = DRV_PLC_THMON_PIN,
+    
+};
+
+/* HAL Interface Initialization for PLC transceiver */
+DRV_PLC_HAL_INTERFACE drvPLCHalAPI = {
+
+    /* PLC PLIB */
+    .plcPlib = &drvPLCPlib,
+
+    /* PLC HAL init */
+    .init = (DRV_PLC_HAL_INIT)DRV_PLC_HAL_Init,
+
+    /* PLC HAL setup */
+    .setup = (DRV_PLC_HAL_SETUP)DRV_PLC_HAL_Setup,
+
+    /* PLC transceiver reset */
+    .reset = (DRV_PLC_HAL_RESET)DRV_PLC_HAL_Reset,
+
+    /* PLC Set StandBy Mode */
+    .setStandBy = (DRV_PLC_HAL_SET_STBY)DRV_PLC_HAL_SetStandBy,
+    
+    /* PLC Get Thermal Monitor value */
+    .getThermalMonitor = (DRV_PLC_HAL_GET_THMON)DRV_PLC_HAL_GetThermalMonitor,
+    
+    /* PLC Set TX Enable Pin */
+    .setTxEnable = (DRV_PLC_HAL_SET_TXENABLE)DRV_PLC_HAL_SetTxEnable,
+    
+    /* PLC HAL Enable/Disable external interrupt */
+    .enableExtInt = (DRV_PLC_HAL_ENABLE_EXT_INT)DRV_PLC_HAL_EnableInterrupts,
+    
+    /* PLC HAL Enable/Disable external interrupt */
+    .getPinLevel = (DRV_PLC_HAL_GET_PIN_LEVEL)DRV_PLC_HAL_GetPinLevel,
+
+    /* PLC HAL delay function */
+    .delay = (DRV_PLC_HAL_DELAY)DRV_PLC_HAL_Delay,
+
+    /* PLC HAL Transfer Bootloader Command */
+    .sendBootCmd = (DRV_PLC_HAL_SEND_BOOT_CMD)DRV_PLC_HAL_SendBootCmd,
+
+    /* PLC HAL Transfer Write/Read Command */
+    .sendWrRdCmd = (DRV_PLC_HAL_SEND_WRRD_CMD)DRV_PLC_HAL_SendWrRdCmd,
+};
+
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="DRV_G3_MACRT Initialization Data">
+
+/* PLC MAC RT Binary file addressing */
+extern uint8_t g3_mac_rt_bin_start;
+extern uint8_t g3_mac_rt_bin_end;
+extern uint8_t g3_mac_rt_bin2_start;
+extern uint8_t g3_mac_rt_bin2_end;
+
+/* G3 MAC RT Driver Initialization Data */
+DRV_G3_MACRT_INIT drvG3MacRtInitData = {
+
+    /* SPI PLIB API interface*/
+    .plcHal = &drvPLCHalAPI,
+ 
+    /* PLC MAC RT Binary start address */
+    .binStartAddress = (uint32_t)&g3_mac_rt_bin_start,
+    
+    /* PLC MAC RT Binary end address */
+    .binEndAddress = (uint32_t)&g3_mac_rt_bin_end,
+
+    /* Secure Mode */
+    .secure = DRV_PLC_SECURE,
+    
+};
+
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="SYS_TIME Initialization Data">
+
+static const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
+    .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)TC0_CH0_TimerCallbackRegister,
+    .timerStart = (SYS_TIME_PLIB_START)TC0_CH0_TimerStart,
+    .timerStop = (SYS_TIME_PLIB_STOP)TC0_CH0_TimerStop ,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)TC0_CH0_TimerFrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)TC0_CH0_TimerPeriodSet,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)TC0_CH0_TimerCompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)TC0_CH0_TimerCounterGet,
+};
+
+static const SYS_TIME_INIT sysTimeInitData =
+{
+    .timePlib = &sysTimePlibAPI,
+    .hwTimerIntNum = TC0_CH0_IRQn,
+};
+
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="SYS_CONSOLE Instance 0 Initialization Data">
+
+
+static const SYS_CONSOLE_UART_PLIB_INTERFACE sysConsole0UARTPlibAPI =
+{
+    .read_t = (SYS_CONSOLE_UART_PLIB_READ)FLEXCOM0_USART_Read,
+    .readCountGet = (SYS_CONSOLE_UART_PLIB_READ_COUNT_GET)FLEXCOM0_USART_ReadCountGet,
+    .readFreeBufferCountGet = (SYS_CONSOLE_UART_PLIB_READ_FREE_BUFFFER_COUNT_GET)FLEXCOM0_USART_ReadFreeBufferCountGet,
+    .write_t = (SYS_CONSOLE_UART_PLIB_WRITE)FLEXCOM0_USART_Write,
+    .writeCountGet = (SYS_CONSOLE_UART_PLIB_WRITE_COUNT_GET)FLEXCOM0_USART_WriteCountGet,
+    .writeFreeBufferCountGet = (SYS_CONSOLE_UART_PLIB_WRITE_FREE_BUFFER_COUNT_GET)FLEXCOM0_USART_WriteFreeBufferCountGet,
+};
+
+static const SYS_CONSOLE_UART_INIT_DATA sysConsole0UARTInitData =
+{
+    .uartPLIB = &sysConsole0UARTPlibAPI,
+};
+
+static const SYS_CONSOLE_INIT sysConsole0Init =
+{
+    .deviceInitData = (const void*)&sysConsole0UARTInitData,
+    .consDevDesc = &sysConsoleUARTDevDesc,
+    .deviceIndex = 0,
+};
+
+
+
+// </editor-fold>
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* MISRAC 2012 deviation block end */
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
+
+    SEFC0_Initialize();
+
+    SEFC1_Initialize();
+  
+    DWDT_Initialize();
+    CLK_Initialize();
+    RSTC_Initialize();
+
+    PIO_Initialize();
+    SUPC_Initialize();
+
+
+
+
+	BSP_Initialize();
+    ADC_Initialize();
+    FLEXCOM5_SPI_Initialize();
+
+ 
+    TC0_CH0_TimerInitialize(); 
+     
+    
+    FLEXCOM0_USART_Initialize();
+
+
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
+    /* Initialize G3 MAC RT Driver Instance */
+    sysObj.drvG3MacRt = DRV_G3_MACRT_Initialize(DRV_G3_MACRT_INDEX, (SYS_MODULE_INIT *)&drvG3MacRtInitData);
+    PIO_PinInterruptCallbackRegister((PIO_PIN)DRV_PLC_EXT_INT_PIN, DRV_G3_MACRT_ExternalInterruptHandler, sysObj.drvG3MacRt);
+
+    /* Initialize PVDD Monitor Service */
+    SRV_PVDDMON_Initialize();
+
+    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
+    H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+        
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+    
+    /* MISRAC 2012 deviation block end */
+    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
+     H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+        sysObj.sysConsole0 = SYS_CONSOLE_Initialize(SYS_CONSOLE_INDEX_0, (SYS_MODULE_INIT *)&sysConsole0Init);
+   /* MISRAC 2012 deviation block end */
+
+
+    /* MISRAC 2012 deviation block end */
+    APP_PLC_Initialize();
+    APP_CONSOLE_Initialize();
+
+
+    NVIC_Initialize();
+
+
+    /* MISRAC 2012 deviation block end */
+}
+
+/*******************************************************************************
+ End of File
+*/
