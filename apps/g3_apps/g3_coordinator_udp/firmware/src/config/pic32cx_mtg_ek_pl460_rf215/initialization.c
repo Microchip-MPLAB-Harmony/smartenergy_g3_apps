c527f180768d277974d321e3a0ea4cedbad90e42
David Sancho <david.sancho@microchip.com>
2025-11-12 12:24:33 +0100

    MH3-115038: Changes after changing BSP to SHD.

--- apps/g3_apps/g3_coordinator_udp/firmware/src/config/pic32cx_mtg_ek_pl460_rf215/initialization.c
+++ apps/g3_apps/g3_coordinator_udp/firmware/src/config/pic32cx_mtg_ek_pl460_rf215/initialization.c
@@ -585,7 +585,7 @@ void SYS_Initialize ( void* data )
 
 	TRNG_Initialize();
 
-	BSP_Initialize();
+    BSP_Initialize();
 
     /* MISRAC 2012 deviation block start */
     /* Following MISRA-C rules deviated in this block  */
