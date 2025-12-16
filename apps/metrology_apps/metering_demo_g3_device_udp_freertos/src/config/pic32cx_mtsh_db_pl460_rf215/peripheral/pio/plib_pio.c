bc5fcfd31e5360950bfc496e0f5d884cb0e1b77e
David Sancho <david.sancho@microchip.com>
2025-12-16 15:42:15 +0100

    MH3-115334: Updated metering_demo app to work with PL460 EK v5.

--- apps/metrology_apps/metering_demo_g3_device_udp_freertos/src/config/pic32cx_mtsh_db_pl460_rf215/peripheral/pio/plib_pio.c
+++ apps/metrology_apps/metering_demo_g3_device_udp_freertos/src/config/pic32cx_mtsh_db_pl460_rf215/peripheral/pio/plib_pio.c
@@ -72,7 +72,7 @@ void PIO_Initialize ( void )
    PIOA_REGS->PIO_CFGR = 0x1U;
 
  /* Port A Peripheral function GPIO configuration */
-   PIOA_REGS->PIO_MSKR = 0x4003c00cLU;
+   PIOA_REGS->PIO_MSKR = 0x4000c00cLU;
    PIOA_REGS->PIO_CFGR = 0x0U;
 
  /* Port A Pin 2 configuration */
@@ -91,20 +91,12 @@ void PIO_Initialize ( void )
    PIOA_REGS->PIO_MSKR = 0x8000U;
    PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x1000200U;
 
- /* Port A Pin 16 configuration */
-   PIOA_REGS->PIO_MSKR = 0x10000U;
-   PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-
- /* Port A Pin 17 configuration */
-   PIOA_REGS->PIO_MSKR = 0x20000U;
-   PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-
  /* Port A Pin 21 configuration */
    PIOA_REGS->PIO_MSKR = 0x200000U;
    PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port A Latch configuration */
-   PIOA_REGS->PIO_CODR = 0x4003c00cLU;
+   PIOA_REGS->PIO_CODR = 0x4000c00cLU;
 
     /* Clear the ISR register */
    (uint32_t)PIOA_REGS->PIO_ISR;
@@ -115,6 +107,10 @@ void PIO_Initialize ( void )
  /* Port B Peripheral function GPIO configuration */
    PIOB_REGS->PIO_MSKR = 0x6000000U;
    PIOB_REGS->PIO_CFGR = 0x0U;
+
+ /* Port B Pin 1 configuration */
+   PIOB_REGS->PIO_MSKR = 0x2U;
+   PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port B Pin 22 configuration */
    PIOB_REGS->PIO_MSKR = 0x400000U;
@@ -157,24 +153,15 @@ void PIO_Initialize ( void )
    PIOC_REGS->PIO_CODR = 0x300000U;
 
  /* Port D Peripheral function GPIO configuration */
-   PIOD_REGS->PIO_MSKR = 0x90008U;
+   PIOD_REGS->PIO_MSKR = 0x80000U;
    PIOD_REGS->PIO_CFGR = 0x0U;
-
- /* Port D Pin 3 configuration */
-   PIOD_REGS->PIO_MSKR = 0x8U;
-   PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-
- /* Port D Pin 16 configuration */
-   PIOD_REGS->PIO_MSKR = 0x10000U;
-   PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port D Pin 19 configuration */
    PIOD_REGS->PIO_MSKR = 0x80000U;
    PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port D Latch configuration */
-   PIOD_REGS->PIO_SODR = 0x10000U;
-   PIOD_REGS->PIO_CODR = 0x90008U & ~0x10000U;
+   PIOD_REGS->PIO_CODR = 0x80000U;
 
 
 
